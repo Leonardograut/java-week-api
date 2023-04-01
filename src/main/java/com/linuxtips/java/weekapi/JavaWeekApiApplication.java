b23e3610401f734150ea5a76fc219b448cfbe672
Leonardo Nascimento <omgleo@live.com>
2023-04-01 13:23:27 -0300

    Projeto Java Week Criado

---
+++ src/main/java/com/linuxtips/java/weekapi/JavaWeekApiApplication.java
@@ -0,0 +1,13 @@
+package com.linuxtips.java.weekapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWeekApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWeekApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linuxtips/java/weekapi/JavaWeekApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linuxtips.java.weekapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWeekApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
